73668737a0f0589a71daab140b20d4350f113d7f
Jose Arias <joseam2969@gmail.com>
2019-09-08 22:57:24 -0400

    Finished Assignment

--- Practice 3/LoginXamarin/ViewModel/HomePageViewModel.cs
+++ Practice 3/LoginXamarin/ViewModel/HomePageViewModel.cs
@@ -14,7 +14,9 @@ namespace LoginXamarin.ViewModel
 		public ContactModel Contact { get; set; }
 		public ObservableCollection<ContactModel> Contacts { get; set; } = new ObservableCollection<ContactModel>();
 		public ICommand AddCommand { get; set; }
-		
+		public ICommand SeeMoreCommand { get; set; }
+		public ICommand DeleteCommand { get; set; }
+
 		public HomePageViewModel(ContactModel contact)
 		{
 			Contact = contact;
@@ -28,6 +30,31 @@ namespace LoginXamarin.ViewModel
 				}));
 				await App.Current.MainPage.Navigation.PushAsync(new AddContactPage(new ContactModel()));
 
+			});
+			SeeMoreCommand = new Command<ContactModel>(async (ExistingContact) =>
+			{
+				string call = "Call this contact";
+				string edit = "Edit this contact";
+				string result = await App.Current.MainPage.DisplayActionSheet("What do you wanna do?", "Cancel", "Ok", call, edit);
+				if(result == call)
+				{
+					Device.OpenUri(new Uri(String.Format("tel:{0}", ExistingContact.PhoneNumber)));
+				}
+				else
+				{
+					await App.Current.MainPage.Navigation.PushAsync(new AddContactPage(ExistingContact));
+				}
+
+
+			});
+			DeleteCommand = new Command<ContactModel>(async (ExistingContact) =>
+			{
+				string warning = "Do yo really wanna delete this contact?";
+				bool response = await App.Current.MainPage.DisplayAlert("This action is irreversible", warning, "Yes", "No");
+				if (response)
+				{
+					Contacts.Remove(ExistingContact);
+				}
 			});
 		}
 	}
